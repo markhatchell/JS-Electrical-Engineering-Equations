adcf86da4b23079dee64aeeee87aa0d8247ccf75
Mark Hatchell <mark.hatchell@prospect33.com>
2018-01-24 17:54:06 -0500

    optimized the wavelength calculation code.

--- Wavelength.js
+++ Wavelength.js
@@ -1,3 +1,12 @@
+'use strict';
+
+
+function metersToCentimeters(meters, precision = 4) {
+  if (typeof precision === 'undefined') {
+    precision = 4;
+  }
+  return (meters * 100).toPrecision(precision);
+}
 
 function metersToFeet(meters, precision = 4) {
   if (typeof precision === 'undefined') {
@@ -13,97 +22,105 @@ function metersToInches(meters, precision = 4) {
   return ((meters * 3.2808399) * 12).toPrecision(precision);
 }
 
-class Wavelength {
-
-  frequencyInMegahertz = 0;
-  valueInMeters = 0;
-  precision = 0;
-
-  constructor(frequencyInMegahertz, precision = 4) {
-    if (typeof frequencyInMegahertz === 'undefined') {
-      throw Error('Need a frequency to init a Wavelength');
-    }
-    if (typeof precision === 'undefined') {
-      precision = 4;
-    }
-    this.frequencyInMegahertz = parseFloat(frequencyInMegahertz);
-    this.valueInMeters = 300 / this.frequencyInMegahertz;
-    this.setPrecision(precision);
+function Wavelength(frequencyInMegahertz, precision = 4) {
+  if (typeof frequencyInMegahertz === 'undefined') {
+    throw Error('Need a frequency to init a Wavelength');
   }
-
-  toString() {
-    return this.valueInMeters.toPrecision(this.precision);
+  if (typeof precision === 'undefined') {
+    precision = 4;
   }
-
-  setPrecision(p) {
-    this.precision = p;
-  }
-
-  toFeet() {
-    return metersToFeet(this.valueInMeters);
-  }
-
-  toInches() {
-    return metersToInches(this.valueInMeters);
-  }
-
-  toThreeQuartersWavelength(unit = 'm') {
-    if (typeof unit === 'undefined') {
-      unit = 'm';
-    }
-    switch(unit) {
-      case 'm':
-        return (this.valueInMeters * .75).toPrecision(this.precision);
-      case 'f':
-        return (metersToFeet(this.valueInMeters * .75)).toPrecision(this.precision);
-      case 'in':
-        return (metersToInches(this.valueInMeters * .75)).toPrecision(this.precision);
-    }
-  }
-
-  toFiveEighthsWavelength(unit = 'm') {
-    if (typeof unit === 'undefined') {
-      unit = 'm';
-    }
-    switch(unit) {
-      case 'm':
-        return (this.valueInMeters * .625).toPrecision(this.precision);
-      case 'f':
-        return (metersToFeet(this.valueInMeters * .625)).toPrecision(this.precision);
-      case 'in':
-        return (metersToInches(this.valueInMeters * .625)).toPrecision(this.precision);
-    }
-  }
-
-  toHalfWavelength(unit = 'm') {
-    if (typeof unit === 'undefined') {
-      unit = 'm';
-    }
-    switch(unit) {
-      case 'm':
-        return (this.valueInMeters * .5).toPrecision(this.precision);
-      case 'f':
-        return (metersToFeet(this.valueInMeters * .5)).toPrecision(this.precision);
-      case 'in':
-        return (metersToInches(this.valueInMeters * .5)).toPrecision(this.precision);
-    }
-  }
-
-  toQuarterWavelength(unit = 'm') {
-    if (typeof unit === 'undefined') {
-      unit = 'm';
-    }
-    switch(unit) {
-      case 'm':
-        return (this.valueInMeters * .25).toPrecision(this.precision);
-      case 'f':
-        return (metersToFeet(this.valueInMeters * .25)).toPrecision(this.precision);
-      case 'in':
-        return (metersToInches(this.valueInMeters * .25)).toPrecision(this.precision);
-    }
-  }
-
+  this.frequencyInMegahertz = parseFloat(frequencyInMegahertz);
+  this.valueInMeters = 300 / this.frequencyInMegahertz;
+  this.setPrecision(precision);
 }
 
+Wavelength.prototype.toString = function() {
+  return this.valueInMeters.toPrecision(this.precision);
+};
+
+Wavelength.prototype.setPrecision = function(p) {
+  this.precision = p;
+};
+
+Wavelength.prototype.toCentimeters = function() {
+  return metersToCentimeters(this.valueInMeters);
+};
+
+Wavelength.prototype.toFeet = function() {
+  return metersToFeet(this.valueInMeters, this.precision);
+};
+
+Wavelength.prototype.toInches = function() {
+  return metersToInches(this.valueInMeters, this.precision);
+};
+
+Wavelength.prototype.toThreeQuartersWavelength = function(unit = 'm') {
+  const threeQuartersWavelength = this.valueInMeters * .75;
+  if (typeof unit === 'undefined') {
+    unit = 'm';
+  }
+
+  switch(unit) {
+    case 'cm':
+      return metersToCentimeters(threeQuartersWavelength, this.precision);
+    case 'm':
+      return (threeQuartersWavelength).toPrecision(this.precision);
+    case 'f':
+      return metersToFeet(threeQuartersWavelength, this.precision);
+    case 'in':
+      return metersToInches(threeQuartersWavelength, this.precision);
+  }
+};
+
+Wavelength.prototype.toFiveEighthsWavelength = function(unit = 'm') {
+  const fiveEighthsWavelength = this.valueInMeters * .625;
+  if (typeof unit === 'undefined') {
+    unit = 'm';
+  }
+  switch(unit) {
+    case 'cm':
+      return metersToCentimeters(fiveEighthsWavelength, this.precision);
+    case 'm':
+      return (fiveEighthsWavelength).toPrecision(this.precision);
+    case 'f':
+      return metersToFeet(fiveEighthsWavelength, this.precision);
+    case 'in':
+      return metersToInches(fiveEighthsWavelength, this.precision);
+  }
+};
+
+Wavelength.prototype.toHalfWavelength = function(unit = 'm') {
+  const halfWavelength = this.valueInMeters * .5;
+  if (typeof unit === 'undefined') {
+    unit = 'm';
+  }
+  switch(unit) {
+    case 'cm':
+      return metersToCentimeters(halfWavelength, this.precision);
+    case 'm':
+      return (halfWavelength).toPrecision(this.precision);
+    case 'f':
+      return metersToFeet(halfWavelength, this.precision);
+    case 'in':
+      return metersToInches(halfWavelength, this.precision);
+  }
+};
+
+Wavelength.prototype.toQuarterWavelength = function(unit = 'm') {
+  const quaterWavelength = this.valueInMeters * .25;
+  if (typeof unit === 'undefined') {
+    unit = 'm';
+  }
+  switch(unit) {
+    case 'cm':
+      return metersToCentimeters(quaterWavelength, this.precision);
+    case 'm':
+      return (quaterWavelength).toPrecision(this.precision);
+    case 'f':
+      return metersToFeet(quaterWavelength, this.precision);
+    case 'in':
+      return metersToInches(quaterWavelength, this.precision);
+  }
+};
 
 export default Wavelength;
