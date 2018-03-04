4cf30e2fcc4fd5f6c86970a97ea6760ae15ea5db
Mark Hatchell <mhatchell@mac.com>
2018-03-04 13:49:00 -0500

    added ResistorDivider to exports.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import OhmsLaw from './src/Ohms-Law';
 import TankCircuit from './src/Tank-Circuit';
 import Battery from './src/Battery';
 import { getFloat, metersToCentimeters, metersToInches, metersToFeet, meterConversion } from './src/util-functions';
+import ResistorDivider from "./src/Resistor-Divider";
 
 module.exports = {
   OhmsLaw,
   Wavelength,
   TankCircuit,
   Battery,
+  ResistorDivider,
   getFloat,
   metersToCentimeters,
   metersToInches,
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import OhmsLaw from './Ohms-Law';
 import TankCircuit from './Tank-Circuit';
 import Battery from './Battery';
 import { getFloat, metersToCentimeters, metersToInches, metersToFeet, meterConversion } from './util-functions';
+import ResistorDivider from "./Resistor-Divider";
 
 module.exports = {
   OhmsLaw,
   Wavelength,
   TankCircuit,
   Battery,
+  ResistorDivider,
   getFloat,
   metersToCentimeters,
   metersToInches,
